c779d005d16a17ffec1e93f24ea2ed80b46709e9
KuoPeiYu <kuopeiyu711@gmail.com>
2017-09-25 11:36:30 +0800

    Create project

---
+++ app/src/main/java/com/example/confirm_identity_app/confirm_identity_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.confirm_identity_app.confirm_identity_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
