45e0d5f86c7b4fc24af16d86b2374bcc82be64b0
susan31213 <susan31213@gmail.com>
2017-10-09 00:31:50 +0800

    add hand input

--- app/src/main/java/com/example/susan/scanner/MainActivity.java
+++ app/src/main/java/com/example/susan/scanner/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity {
 
     private TextView scanResult;
+    private EditText idInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         scanResult = (TextView)findViewById(R.id.textView3);
         scanResult.setText("");
+        idInput = (EditText)findViewById(R.id.editText);
+        Button addBtn = (Button)findViewById(R.id.add);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isValidId(idInput.getText().toString())) {
+                    Toast.makeText(MainActivity.this, idInput.getText().toString(), Toast.LENGTH_LONG).show();
+                    scanResult.append(idInput.getText().toString() + "\n");
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Not a student ID.", Toast.LENGTH_LONG).show();
+            }
+        });
 
         Button startBtn = (Button)findViewById(R.id.scanner);
         final Activity activity = this;
@@ -42,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null)
@@ -53,8 +75,14 @@ public class MainActivity extends AppCompatActivity {
             else
             {
                 String content = result.getContents();
-                Toast.makeText(this, content, Toast.LENGTH_LONG).show();
-                scanResult.append(content + "\n");
+                if(isValidId(content))
+                {
+                    Toast.makeText(this, content, Toast.LENGTH_LONG).show();
+                    scanResult.append(content + "\n");
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Not a student ID.", Toast.LENGTH_LONG).show();
+
             }
         }
         else
@@ -68,12 +96,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState)
     {
         outState.putString("textView", scanResult.getText().toString());
+        outState.putString("editText", idInput.getText().toString());
     }
 
     @Override
     protected  void onRestoreInstanceState(Bundle savedInstanceState)
     {
         scanResult.setText(savedInstanceState.getString("textView"));
+        idInput.setText(savedInstanceState.getString("editText"));
+    }
+
+    private boolean isValidId(String id)
+    {
+        if(id.charAt(0) >= 'A' && id.charAt(0) <= 'Z')
+        {
+            for(int i=1; i<9; i++)
+                if(id.charAt(i) < '0' || id.charAt(i) > '9')
+                    return false;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
