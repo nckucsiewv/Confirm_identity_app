2cb9f772baf119c6319e3a3240482e7773bb3e87
zeroplusone <a711186@gmail.com>
2017-10-18 04:24:27 +0800

    delete function of inserting users

--- app/src/main/java/csiewv/yuwen/app/app/MainActivity.java
+++ app/src/main/java/csiewv/yuwen/app/app/MainActivity.java
@@ -1,10 +1,8 @@
 package csiewv.yuwen.app.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,13 +13,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import csiewv.yuwen.app.app.R;
-
-
 public class MainActivity extends AppCompatActivity {
 
     public DatabaseReference myRef;
-    static public int count=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,54 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
-    }
-
-    // called when users clicking on the 'leader' checkBox
-    // if the leader checkBox is checked, enable the phoneNum editText,
-    // so that user can input leaders' phoneNum
-    public void onCheckBoxClicked(View v){
-        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox_leader);
-        EditText editText_phone = (EditText) findViewById(R.id.editText_phone);
-        if(checkBox.isChecked()){
-            editText_phone.setEnabled(true);
-        }else{
-            editText_phone.setEnabled(false);
-        }
-    }
-
-    // add a player info to firebase
-    public void addPlayer(String name, String id){
-        Player player = new Player(name,id);
-        myRef.child("player").child(Integer.toString(count)).setValue(player);
-        ++count;
-    }
-
-    // add a leader info to firebase
-    public void addLeader(String name, String id, String phone){
-        Player player = new Player(name,id,phone);
-        myRef.child("player").child(Integer.toString(count)).setValue(player);
-        ++count;
-    }
-
-    // called when users clicking the 'send' btn
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayActivity.class);
-
-        EditText editText = (EditText) findViewById(R.id.editText_name);
-        String toSend_name = editText.getText().toString();
-        EditText editText2 = (EditText) findViewById(R.id.editText_num);
-        String toSend_id = editText2.getText().toString();
-
-        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox_leader);
-        if(checkBox.isChecked()){
-            EditText editText3 = (EditText) findViewById(R.id.editText_phone);
-            String toSend_phone = editText3.getText().toString();
-            addLeader(toSend_name,toSend_id,toSend_phone);
-        }else{
-            addPlayer(toSend_name,toSend_id);
-        }
-
-        startActivity(intent);
     }
 
     // called when users clicking the 'check id' btn
